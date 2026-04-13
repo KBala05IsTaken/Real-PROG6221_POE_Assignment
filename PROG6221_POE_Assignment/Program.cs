3c0f748e55315579803143dce49d2978c8b7dfa9
KBala05IsTaken <ST10474385@rcconnect.edu.za>
2026-04-13 22:06:09 +0200

    Asking for your name is now it's own method Aded colour to some of the console texts

--- PROG6221_POE_Assignment/Program.cs
+++ PROG6221_POE_Assignment/Program.cs
@@ -1,6 +1,8 @@
 ﻿//
 using System;
 using System.Data;
+using System.Media;
+using System.Xml.Linq;
 
 class program
 {
@@ -19,13 +21,10 @@ class program
         Console.WriteLine("                                           ");
         Console.ResetColor();
         Console.WriteLine("        AI Assistant");
+        String sName = "";
 
-        Console.WriteLine("------------------------------------");
-        Console.WriteLine("");
-        Console.Write("What is your name? ");
-        String sName = Console.ReadLine();
-        Console.WriteLine("");
-        Console.WriteLine("Wow, " + sName + " Sounds like a great name");
+        InputtingName(sName);
+        
 
         //Options
         Console.WriteLine("------------------------------------");
@@ -33,9 +32,11 @@ class program
         Console.WriteLine("Alright then " + sName + ", What would you like to do? (Only type in the number on your keyboard)");
         Console.WriteLine();
 
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("(1) How do you feel?");
         Console.WriteLine("(2) What is your purpose?");
         Console.WriteLine("(3) What can I ask of you, regarding cybersecurity?");
+        Console.ResetColor();
         Console.Write("Enter here: ");
         String sAnswer1 = Console.ReadLine();
         Console.WriteLine("");
@@ -44,14 +45,18 @@ class program
         {
             case "1":
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("I feel great. Thank you so much for asking me " +sName+ 
                     ". I hope that as your AI assistant that I can be helpful to you.");
+                Console.ResetColor();
                 break;
 
             case "2":
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("My purpose as an AI Assistant is to to help you be more aware of the dangers of the " +
                     "internet in terms of Cybersecurity, " +sName+ ".");
+                Console.ResetColor();
                 break;
 
             case "3":
@@ -59,9 +64,11 @@ class program
                 Console.WriteLine("Okay then " +sName+ ", what would you like to know about?");
                 Console.WriteLine("");
 
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("(1) Password safety");
                 Console.WriteLine("(2) Phishing");
                 Console.WriteLine("(3) Safe browsing");
+                Console.ResetColor();
                 Console.Write("Enter here: ");
                 String sAnswer2 = Console.ReadLine();
                 Console.WriteLine("");
@@ -70,25 +77,31 @@ class program
                 {
                     case "1":
                         Console.WriteLine("------------------------------------");
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine("Password safety refers to the collection of practices and " +
                             "technologies used to protect your accounts from unauthorized access. " +
                             "It serves as your primary line of defense against hackers who attempt to steal " +
                             "personal information, identity, or money.");
+                        Console.ResetColor();
                         break;
 
                     case "2":
                         Console.WriteLine("------------------------------------");
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine("Phishing is a cyberattack where scammers impersonate trusted " +
                             "entities via email, text, or phone to steal sensitive information—passwords, " +
                             "credit card numbers, or bank details. Attackers use urgent, alarming messages to " +
                             "trick victims into clicking malicious links, downloading malware, or visiting fake websites.");
+                        Console.ResetColor();
                         break;
 
                     case "3":
                         Console.WriteLine("------------------------------------");
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.WriteLine("Safe Browsing launched in 2005 to protect users across the web from phishing " +
                             "attacks, and has evolved to give users tools to help protect themselves from web-based threats " +
                             "like malware, unwanted software, and social engineering across desktop and mobile platforms.");
+                        Console.ResetColor();
                         break;
                 }
                 break;
@@ -103,6 +116,15 @@ class program
 
 
         Console.WriteLine("===================================");
+    }
+    public static void InputtingName(string name) 
+    {
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("");
+        Console.Write("What is your name? ");
+        name = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine("Wow, " + name + " Sounds like a great name");
     }
 }
 
